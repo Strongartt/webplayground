2c4ae90ee8d4a7221347246014fcb15086da4c95
Strongartt <rikardoperez0809@gmail.com>
2025-04-07 18:22:10 -0500

    Aplicacion de Mixin para staff

--- pages/views.py
+++ pages/views.py
@@ -21,20 +21,23 @@ class PagesListView(ListView):
 
 class PageDetailView(DetailView):
     model = Page
-    
-class PageCreate(StaffRequiredMixin, CreateView):
+ 
+@method_decorator(staff_member_required, name='dispatch')   
+class PageCreate(CreateView):
     model = Page
     fields = ['title','content','order']
     success_url = reverse_lazy('pages:pages')
     
-class PageUpdate(StaffRequiredMixin, UpdateView):
+@method_decorator(staff_member_required, name='dispatch')       
+class PageUpdate(UpdateView):
     model = Page
     fields = ['title','content','order']
     template_name_suffix = '_update_form'
 
     def get_success_url(self):
         return reverse_lazy ('pages:update', args=[self.object.id]) + '?ok'
-
-class PageDelete(StaffRequiredMixin, DeleteView):
+    
+@method_decorator(staff_member_required, name='dispatch')   
+class PageDelete(DeleteView):
     model = Page
     success_url = reverse_lazy("pages:pages")
